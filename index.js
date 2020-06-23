303ca37948a2d528574d29b808f8e1dcbcae515d
Tanmay Anand <tanmayanand@gmail.com>
2020-06-23 19:18:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes={breakfast:"eggs"};
 
 function updateObjectWithKeyAndValue(object, key, value){
-    object[key]=value;
-    return object;
+    return Object.assign({},object,{[key]:value});
 }
